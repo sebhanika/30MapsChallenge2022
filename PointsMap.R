3225f672cfbaeed389a8464812b1a2d79c4dc00b
sebhanika <hanikasebastian@gmail.com>
2022-10-01 23:01:37 +0200

    Create project

---
+++ PointsMap.R
@@ -0,0 +1,20 @@
+## ---------------------------
+##
+## Title: 
+##
+## Topic:
+##
+## Author: Sebastian Hanika
+##
+## Date Created: 2022-10-01
+##
+## ---------------------------
+##
+## Notes:
+##   
+##
+## ---------------------------
+
+
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
