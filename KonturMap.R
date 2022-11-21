3556efcb9ad97ce97928c7d6eafa20d43663854a
Sebastian Hanika <se2627ha@lu.se>
2022-11-21 13:53:23 +0100

    Fix

--- KonturMap.R
+++ KonturMap.R
@@ -25,11 +25,6 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()))
 
 # Libraries and setup -----------------------------------------------------
 
-list.of.packages <- c("tidyverse", "rgdal", sf, downloader)
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
-
-
 library(tidyverse)
 library(rgdal)
 library(sf)
